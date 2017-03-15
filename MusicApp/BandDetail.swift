986d3920147e91d87cf00ec43d9e56025f2fa3b7
Brian Marshall <bmarshall@RIIS.com>
2017-03-15 13:37:36 -0400

    first commit

---
+++ MusicApp/BandDetail.swift
@@ -0,0 +1,13 @@
+//
+//  BandDetail.swift
+//  MusicApp
+//
+//  Created by Brian Marshall on 3/14/17.
+//  Copyright © 2017 Brian Marshall. All rights reserved.
+//
+
+import UIKit
+
+class BandDetail: NSObject {
+
+}
---
+++ MusicApp/BandsModel.swift
@@ -0,0 +1,13 @@
+//
+//  BandsModel.swift
+//  MusicApp
+//
+//  Created by Brian Marshall on 3/15/17.
+//  Copyright © 2017 Brian Marshall. All rights reserved.
+//
+
+import UIKit
+
+class BandsModel: NSObject {
+
+}
